21b6930a2d7b7d213520595ff37f2afd2ee4286a
mtmattqq <124585214+mtmattqq@users.noreply.github.com>
2024-03-14 11:45:02 +0800

    Test Functionality of CTk

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class App(customtkinter.CTk):
         super().__init__()
 
         self.title("my app")
-        self.geometry("400x150")
+        self.geometry("400x200")
         self.grid_columnconfigure((0, 1), weight=1)
 
         self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
@@ -14,9 +14,17 @@ class App(customtkinter.CTk):
         self.checkbox_1.grid(row=1, column=0, padx=20, pady=(0, 20), sticky="w")
         self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
         self.checkbox_2.grid(row=1, column=1, padx=20, pady=(0, 20), sticky="w")
+
+        self.font = customtkinter.CTkFont(family="Cascadia Code", size=16)
+        self.textbox = customtkinter.CTkTextbox(self, width=200, font=self.font, wrap="word")
+        self.textbox.tag_config("1", foreground="red")
+        self.textbox.insert(index=0.0, text="red color", tags="1")
+        
+        self.textbox.grid(row=2, column=1, padx=20, pady=(0, 20), sticky="w")
         
     def button_callback(self):
         print("button pressed")
+        
 
 app = App()
 app.mainloop()
