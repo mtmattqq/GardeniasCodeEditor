c3f880dcacfd4f67efeecd8bd219faa79d5df2cc
mtmattqq <124585214+mtmattqq@users.noreply.github.com>
2024-03-24 13:57:11 +0800

    Add documentation to rep_test_result

--- src/util/test.py
+++ src/util/test.py
@@ -1,5 +1,8 @@
 from colorama import Fore, Back, Style
 
+"""
+A beautiful way to print the test Result.
+"""
 def rep_test_result(msg: str, condi: bool) -> str:
     PASS = f'Test: {msg:30s} [ {Fore.GREEN}Pass{Fore.RESET} ]'
     FAIL = f'Test: {msg:30s} [ {Fore.RED}Fail{Fore.RESET} ]'
